08538ca5c05917f806d8ed1dca2bfdce833d572d
Salguento <salguento@gmail.com>
2024-10-26 09:39:40 -0300

    add css and runtime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: [
-    '@nuxt/eslint',
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/color-mode',
-    '@nuxt/image'
-  ]
-})
+    "@nuxt/eslint",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@nuxt/image",
+  ],
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseKey: process.env.SUPABASE_KEY,
+    },
+  },
+  ssr: false,
+  css: ["~/assets/css/main.css"],
+});
