b307de41008bd7d2a72126da25bfe1f8b9068b76
Salguento <salguento@gmail.com>
2024-10-26 09:39:16 -0300

    add custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,12 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Asap"],
+        display: ["Barriecito"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
